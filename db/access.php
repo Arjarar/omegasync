1ee649ae67a41d36abb7e3afc2eedce503496e5c
Arjarar <arjara@alumnos.uai.cl>
2015-06-23 23:18:13 -0300

    Update access.php

--- db/access.php
+++ db/access.php
@@ -1,1 +1,13 @@
-<?php
+ <?php
+$capabilities = array(
+
+    'local/omegasync:adminview' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+        		'student'=>CAP_PROHIBIT,
+        		'teacher' => CAP_PROHIBIT,
+        		'editingteacher' => CAP_PROHIBIT,
+        		'manager' => CAP_ALLOW
+)));
+ ?>
